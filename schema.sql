3d133a25b58190e75839307149090dc5688d362f
Oleg Zaezdny <oleg.zaezdny@gmail.com>
2014-07-25 10:26:26 +0300

    Many-to-many relationship between books and authors

--- schema.sql
+++ schema.sql
@@ -10,12 +10,17 @@ CREATE TABLE `authors` (
 CREATE TABLE `books` (
   `isbn` varchar(32) NOT NULL DEFAULT '',
   `title` varchar(1024) NOT NULL DEFAULT '',
-  `id_author` int(10) unsigned NOT NULL,
   `published` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `rating` float(5,2) DEFAULT '0.00',
   PRIMARY KEY (`isbn`),
   KEY `title` (`title`(255)),
   KEY `published` (`published`),
-  KEY `authors_id_author` (`id_author`),
-  CONSTRAINT `authors_id_author` FOREIGN KEY (`id_author`) REFERENCES `authors` (`id_author`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `books_authors` (
+  `isbn` VARCHAR(32) NOT NULL,
+  `id_author` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`isbn`, `id_author`),
+   CONSTRAINT `books_isbn` FOREIGN KEY (`isbn`) REFERENCES `books` (`isbn`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `authors_id_author` FOREIGN KEY (`id_author`) REFERENCES `authors` (`id_author`) ON DELETE CASCADE ON UPDATE CASCADE
+) engine=InnoDb;
